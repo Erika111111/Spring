9c8e8f7247b2b22b1b256bd33a3ae1d9e9c9a9d0
Erika <Erika1111@yandex.ru>
2024-02-28 16:41:45 +0300

    HW2

---
+++ seminar2_SpringCRUD/src/test/java/com/example/seminar2_SpringCRUD/Seminar2SpringCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seminar2_SpringCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seminar2SpringCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
